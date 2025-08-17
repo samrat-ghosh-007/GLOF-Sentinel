985f467f06caaf3c72b9180bf1a4cb111cf66c3c
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2025-08-17 11:54:23 +0530

    Removed old backend folder

--- Glof-Backend/models/Alert.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const alertSchema = new mongoose.Schema({
-  lake: { type: mongoose.Schema.Types.ObjectId, ref: 'Lake' },
-  message: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Alert', alertSchema);
--- Glof-Backend/routes/alertRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getAlerts } = require('../controllers/alertController');
-const router = express.Router();
-
-router.get('/', getAlerts);
-
-module.exports = router;
