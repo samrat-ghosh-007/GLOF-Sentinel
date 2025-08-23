72810d942b668d939f4cab9101dbe7de9e738dce
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2025-08-23 22:49:54 +0530

    Fix backend crash by correcting React Router paths and updating server static/frontend serving(2)

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,6 @@ app.use(express.json());
 
 
 
-
-
 const seedRecipients = require('./seed/seedRecipients');
 seedRecipients();
 
@@ -31,11 +29,12 @@ app.use('/api/lakes', require('./routes/lakeRoutes'));
 app.use('/api/alerts', require('./routes/alertRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
 
-// app.use(express.static(path.join(__dirname, 'Glof-Frontend/dist')));
+app.use(express.static(path.join(__dirname, 'Glof-Frontend/dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'Glof-Frontend/dist', 'index.html'));
-// });
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'Glof-Frontend/dist', 'index.html'));
+});
+
 
 cron.schedule('*/30 * * * *', async () => {
   try {
